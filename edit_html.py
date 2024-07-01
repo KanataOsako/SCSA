2ceed73ad1b52d95c13dc25cb85a933e29ab46fe
KanataOsako <kanata-osako@outlook.jp>
2024-07-01 13:59:02 +0900

    html変換箇所追記

--- edit_html.py
+++ edit_html.py
@@ -1,8 +1,8 @@
-from html_to_markdown import html_to_markdown
+from html_to_markdown import html_to_markdown2
 
 def edit_html():
     # HTMLをMarkdownに変換
-    markdown_output = html_to_markdown(open('./output.html','r',encoding="utf-8"))
+    markdown_output = html_to_markdown2(open('./output.html','r',encoding="utf-8"))
     # 取得したHTMLデータをファイルに書き込む
     file_path = './output.md'
     with open(file_path, 'w', encoding='utf-8') as f:
