6506216fc6acbabffa2252fe1911ffb48dff3fc8
kanata-osako <kanata-osako@kccs.co.jp>
2024-07-01 16:48:22 +0900

    markdown変換処理修正

--- html_to_markdown.py
+++ html_to_markdown.py
@@ -18,20 +18,36 @@ def html_to_markdown2(html_content):
     soup = BeautifulSoup(html_content, 'html.parser')
     # 残りのHTMLをマークダウンに変換
     markdown_content = md(str(soup), heading_style="ATX")
-    mrkdwn_content = convert__markdown_to_mrkdwn_2(markdown_content)
+    mrkdwn_content = convert_markdown_to_mrkdwn(markdown_content)
     return mrkdwn_content
 
 def convert_markdown_to_mrkdwn(markdown_text):
-    # 太字
-    markdown_text = re.sub(r'\*\*(.*?)\*\*', r'*\1*', markdown_text)
-    # 斜体
-    markdown_text = re.sub(r'\*(.*?)\*', r'_\1_', markdown_text)
-    # リスト
-    markdown_text = re.sub(r'^- ', r'• ', markdown_text, flags=re.MULTILINE)
+    # 見出し
+    text = re.sub(r'(^|\n)###### (.*)', r'\1* \2 *', text)
+    text = re.sub(r'(^|\n)##### (.*)', r'\1* \2 *', text)
+    text = re.sub(r'(^|\n)#### (.*)', r'\1* \2 *', text)
+    text = re.sub(r'(^|\n)### (.*)', r'\1* \2 *', text)
+    text = re.sub(r'(^|\n)## (.*)', r'\1* \2 *', text)
+    text = re.sub(r'(^|\n)# (.*)', r'\1* \2 *', text)
+    
+    # 強調（太字）
+    text = re.sub(r'\*\*(.*?)\*\*', r'*\1*', text)
+    text = re.sub(r'__(.*?)__', r'*\1*', text)
+    
+    # 強調（斜体）
+    text = re.sub(r'\*(.*?)\*', r'_\1_', text)
+    text = re.sub(r'_(.*?)_', r'_\1_', text)
+    
+    # コードブロック
+    text = re.sub(r'```(.*?)```', r'```\1```', text, flags=re.DOTALL)
+    
+    # インラインコード
+    text = re.sub(r'`(.*?)`', r'`\1`', text)
+    
     # リンク
-    markdown_text = re.sub(r'\[(.*?)\]\((.*?)\)', r'<\2|\1>', markdown_text)
-    return markdown_text
-
+    text = re.sub(r'\[(.*?)\]\((.*?)\)', r'<\2|\1>', text)
+    
+    return text
 
 def convert__markdown_to_mrkdwn_2(markdown_text):
     parser = commonmarkslack.Parser()
