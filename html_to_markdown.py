16490ebda3bad9c4f2a947c53531316afc3067e8
kanata-osako <kanata-osako@kccs.co.jp>
2024-07-02 17:37:45 +0900

    slackのマークダウン形式に直す処理を追加

--- html_to_markdown.py
+++ html_to_markdown.py
@@ -3,6 +3,7 @@ from markdownify import markdownify as md
 import html2text
 import re
 import commonmarkslack
+from convert_markdown_to_mrkdwn import convert_markdown_to_slack_format
 
 def html_to_markdown(html_content):
     # BeautifulSoupを使ってHTMLをパースする
@@ -10,7 +11,7 @@ def html_to_markdown(html_content):
 
     # html2textを使用してHTMLをMarkdownに変換する
     markdown_content = html2text.html2text(str(soup))
-    mrkdwn_content = convert_markdown_to_mrkdwn(markdown_content)
+    mrkdwn_content = convert_markdown_to_slack_format(markdown_content)
     return mrkdwn_content
 
 def html_to_markdown2(html_content):
@@ -18,40 +19,12 @@ def html_to_markdown2(html_content):
     soup = BeautifulSoup(html_content, 'html.parser')
     # 残りのHTMLをマークダウンに変換
     markdown_content = md(str(soup), heading_style="ATX")
-    mrkdwn_content = convert_markdown_to_mrkdwn(markdown_content)
+    mrkdwn_content = convert_markdown_to_slack_format(markdown_content)
     return mrkdwn_content
 
-def convert_markdown_to_mrkdwn(markdown_text):
-    # 見出し
-    text = re.sub(r'(^|\n)###### (.*)', r'\1* \2 *', text)
-    text = re.sub(r'(^|\n)##### (.*)', r'\1* \2 *', text)
-    text = re.sub(r'(^|\n)#### (.*)', r'\1* \2 *', text)
-    text = re.sub(r'(^|\n)### (.*)', r'\1* \2 *', text)
-    text = re.sub(r'(^|\n)## (.*)', r'\1* \2 *', text)
-    text = re.sub(r'(^|\n)# (.*)', r'\1* \2 *', text)
-    
-    # 強調（太字）
-    text = re.sub(r'\*\*(.*?)\*\*', r'*\1*', text)
-    text = re.sub(r'__(.*?)__', r'*\1*', text)
-    
-    # 強調（斜体）
-    text = re.sub(r'\*(.*?)\*', r'_\1_', text)
-    text = re.sub(r'_(.*?)_', r'_\1_', text)
-    
-    # コードブロック
-    text = re.sub(r'```(.*?)```', r'```\1```', text, flags=re.DOTALL)
-    
-    # インラインコード
-    text = re.sub(r'`(.*?)`', r'`\1`', text)
-    
-    # リンク
-    text = re.sub(r'\[(.*?)\]\((.*?)\)', r'<\2|\1>', text)
-    
-    return text
-
-def convert__markdown_to_mrkdwn_2(markdown_text):
-    parser = commonmarkslack.Parser()
-    renderer = commonmarkslack.SlackRenderer()
-    ast = parser.parse(markdown_text.message.content.strip())
-    slack_md = renderer.render(ast)
-    return slack_md
+# def convert__markdown_to_mrkdwn_2(markdown_text):
+#     parser = commonmarkslack.Parser()
+#     renderer = commonmarkslack.SlackRenderer()
+#     ast = parser.parse(markdown_text.message.content.strip())
+#     slack_md = renderer.render(ast)
+#     return slack_md
