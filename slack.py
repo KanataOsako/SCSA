691878bfb6424f9b6754c8c06b17d6f4e20323e9
kanata-osako <kanata-osako@kccs.co.jp>
2024-07-11 10:16:47 +0900

    no message

--- slack.py
+++ slack.py
@@ -4,9 +4,9 @@ from get_confluence_search_result import get_confluence_search_result
 from post_slack import post_slack
 from slack_bolt import App
 from convert_html_to_slack_mrkdwn import convert_html_to_slack_mrkdwn
+from dotenv import load_dotenv
 
-# jsonファイル名
-JSON_FILE_NAME = "user_config.json"
+load_dotenv()
 
 
 # ボットトークンと署名シークレットを使ってアプリを初期化します
@@ -16,43 +16,23 @@ app = App(
 )
 
 
-@app.message()
-def main(message):
-    # jsonファイルから設定値を取得
-    dict_user_config = get_user_config(JSON_FILE_NAME)
-    # confluence検索処理
-    get_confluence_search_result(message["text"], dict_user_config)
-    # html→slackのmrkdwnに変換処理
-    convert_html_to_slack_mrkdwn(dict_user_config)
-    # slack通知処理
-    post_slack(dict_user_config)
-
-
-# Start your app
-if __name__ == "__main__":
-    app.start(port=int(os.environ.get("PORT", 3000)))
-
-
-def get_user_config(json_file_name):
-    # jsonファイル読み込み
-    json_open = open(json_file_name, "r")
-    json_load = json.load(json_open)
+def get_user_config():
     # 接続先confluenceURL
-    confluence_url = json_load["CONFLUENCE_URL"]
+    confluence_url = os.environ["CONFLUENCE_URL"]
     # メールアドレス
-    mail_ad = json_load["MAIL_AD"]
+    mail_ad = os.environ["MAIL_AD"]
     # ユーザーID
-    user_id = json_load["USER_ID"]
+    user_id = os.environ["USER_ID"]
     # パスワード
-    password = json_load["PASSWORD"]
+    password = os.environ["PASSWORD"]
     # output用htmlファイルパス
-    file_path_html = json_load["FILE_PATH_HTML"]
+    file_path_html = os.environ["FILE_PATH_HTML"]
     # output用markdownファイルパス
-    file_path_markdown = json_load["FILE_PATH_MARKDOWN"]
+    file_path_markdown = os.environ["FILE_PATH_MARKDOWN"]
     # slackトークン
-    slack_token = json_load["SLACK_TOKEN"]
+    slack_token = os.environ["SLACK_TOKEN"]
     # slackチャンネルID
-    slack_channel_id = json_load["SLACK_CHANNEL_ID"]
+    slack_channel_id = os.environ["SLACK_CHANNEL_ID"]
 
     dict_user_config = {
         "confluence_url": confluence_url,
@@ -65,3 +45,20 @@ def get_user_config(json_file_name):
         "slack_channel_id": slack_channel_id,
     }
     return dict_user_config
+
+
+@app.message()
+def main(message):
+    # jsonファイルから設定値を取得
+    dict_user_config = get_user_config()
+    # confluence検索処理
+    get_confluence_search_result(message["text"], dict_user_config)
+    # html→slackのmrkdwnに変換処理
+    convert_html_to_slack_mrkdwn(dict_user_config)
+    # slack通知処理
+    post_slack(dict_user_config)
+
+
+# Start your appqqq 
+if __name__ == "__main__":
+    app.start(port=int(os.environ.get("PORT", 3000)))
